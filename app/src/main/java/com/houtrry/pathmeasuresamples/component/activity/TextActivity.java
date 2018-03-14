3ce11c4d28583f99fb766c0121061ec30322430e
houtrry <houtrry@hotmail.com>
2018-03-14 10:01:08 +0800

    录制gif图

--- app/src/main/java/com/houtrry/pathmeasuresamples/component/activity/TextActivity.java
+++ app/src/main/java/com/houtrry/pathmeasuresamples/component/activity/TextActivity.java
@@ -2,6 +2,7 @@ package com.houtrry.pathmeasuresamples.component.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.houtrry.pathmeasuresamples.R;
 import com.houtrry.pathmeasuresamples.component.widget.TextPathView;
@@ -16,6 +17,12 @@ public class TextActivity extends AppCompatActivity {
         setContentView(R.layout.activity_text);
 
         mTextPathView = (TextPathView) findViewById(R.id.textPathView);
-        mTextPathView.startAnimator();
+        mTextPathView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextPathView.startAnimator();
+            }
+        });
+
     }
 }
